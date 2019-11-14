31c81b75eaed340945b20ce71643572bdb6d1af0
foreverZ <617754841@qq.com>
2019-11-14 16:28:29 +0800

    增加响应式

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.scss';
 import App from './App';
+import flexable from './utils/flexable';
 import * as serviceWorker from 'config/serviceWorker';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('测试版');
   console.log(process.env.REACT_APP_VERSION);
 }
+
+flexable(750, 750); // 响应式，75rem = 750px
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/utils/flexable.js
@@ -0,0 +1,27 @@
+
+/**
+ * 页面响应式
+ * designWidth 为设计稿尺寸
+ * remRatio 代表多少 rem 等于屏宽
+ */
+function flexable(designWidth = 750, remRatio = 10) {
+  function e() {
+    const winW = a.getBoundingClientRect().width;
+    console.log(winW, o, d)
+    r.innerText = "html{font-size:" + (a.style.fontSize = winW / o * d + "px") + " !important;}"
+  }
+  var t = navigator.userAgent,
+      n = (t.match(/(iPhone|iPad|iPod)/), t.match(/Android/i), window),
+      i = document,
+      a = i.documentElement,
+      o = (n.devicePixelRatio, designWidth),
+      d = designWidth / remRatio,
+      r = (i.head.querySelector('[name="viewport"]'), i.createElement("style"));
+  r.innerText = "html{font-size:100px !important}";
+  i.head.appendChild(r);
+  e();
+  n.addEventListener("resize", e, !1);
+  a.className += t.match(/ucbrowser/i) ? " app-uc " : "";
+}
+
+export default flexable;
